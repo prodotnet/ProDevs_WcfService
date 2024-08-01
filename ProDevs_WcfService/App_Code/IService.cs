579994fdbd2023052502e68f05ac58552f5454f5
Pro Dot Net <sphesihlengidi2@gmail.com>
2024-08-02 00:41:47 +0200

    Add project files.

---
+++ ProDevs_WcfService/App_Code/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	bool Register(string firstName, string lastName, string email, string password, string gender ,string userType, DateTime createDate);
+
+	[OperationContract]
+	
+	int Login(string email, string password);
+}
+
+
---
+++ ProDevs_WcfService/App_Code/Service.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    DataClassesDataContext data = new DataClassesDataContext();
+
+    public int Login(string email, string password)
+    {
+
+        var login = (from s in data.UserRegistrations where s.Email.Equals(email) && s.Password.Equals(password) select s).FirstOrDefault();
+
+        if (login != null)
+        {
+            return login.Id;
+
+        }
+        else
+        {
+
+            return 0;
+        }
+    }
+
+
+    public bool Register(string firstName, string lastName, string email, string password,  string gender, string userType, DateTime createDate)
+    {
+        
+
+        // Check if the user already exists
+        var IsUserExit = data.UserRegistrations.FirstOrDefault(x => x.Email == email);
+        if (IsUserExit != null)
+        {
+            return false; // User already exists
+        }
+
+        // Create a new user registration
+        var newUser = new UserRegistration
+        {
+           
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password,
+            Gender = gender,
+            UserType = userType,
+            CreateDate = createDate
+        };
+
+        data.UserRegistrations.InsertOnSubmit(newUser);
+
+        try
+        {
+            data.SubmitChanges(); 
+            return true; 
+        }
+        catch (Exception)
+        {
+            return false; 
+        }
+    }
+}
